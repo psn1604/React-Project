59d71bd7ed1b72948d4ad785d5362f4828858824
Pranav Nahar <omnahar16@gmail.com>
2025-03-01 23:56:03 +0530

    part-01

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
-const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am an H1 tag"),React.createElement("h2",{},"I am an H2 tag")]));
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },[React.createElement("h1", {}, "This is new Heading"),
+        [React.createElement("h2", {}, "I am an H2 tag"),
+        React.createElement("h3", {}, "I am an H5 tag")]]));
 
 
 
 
 
-const heading= React.createElement("h1",{id:"heading"},"Hello from React" );
-const root=ReactDOM.createRoot(document.getElementById("root"));
+const heading = React.createElement("h1", { id: "heading" }, "Hello from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
